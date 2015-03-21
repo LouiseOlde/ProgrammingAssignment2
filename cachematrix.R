107a6508480548812cb04b1e40ee72d70d1a2d56
LouiseOlde <louise.olde@gmail.com>
2015-03-21 15:25:56 +0000

    message

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,43 @@
-## Put comments here that give an overall description of what your
-## functions do
+## These functions are used to cache results of a function, then look it up again.  
 
-## Write a short comment describing this function
+## Need to do this first, to access the MASS package to do get "ginv", 'generalized inverse of a matrix' function.
+library(MASS)
 
-makeCacheMatrix <- function(x = matrix()) {
 
+## This function creates a special matrix, which is really a list containing a function to
+# set the value of the matrix
+# get the value of the matrix
+# set the values of the inverse of the matrix
+# get the values of the inverse of the matrix
+makeCacheMatrix <- function(x = matrix()) {       
+  m <- NULL
+  set <- function(y) {
+    x <<- y
+    m <<- NULL
+  }
+  get <- function() x
+  setginv <- function(ginv) m <<- ginv
+  getginv <- function() m
+  list(set = set, get = get,
+       setginv = setginv,
+       getginv = getginv)
 }
 
 
-## Write a short comment describing this function
+## First need to run above code on matrix, and assign it to a new object.
+# Then this function will first check if the inverse of the matrix has already been 
+# calculated. If so, it retrieves the cached inverse. If not, it calculates the inverse.
+
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  m <- x$getginv()
+  if(!is.null(m)) {
+    message("getting cached data")
+    return(m)
+  }
+  data <- x$get()
+  m <- ginv(data, ...)
+  x$setginv(m)
+  m
 }
+
